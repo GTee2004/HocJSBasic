5048c0382665b6aed1a4a21fd0949022398b54b6
GTee2004 <giathanh232004@gmail.com>
2025-06-02 13:33:10 +0700

    Bai4 - String

---
+++ Bai4/Bai4.js
@@ -0,0 +1,14 @@
+let a = 'Học về String '
+console.log(a)
+console.log( 4 + 5 + 'a') // 9a
+console.log('a' + 4 + 5 + 6) // a456
+console.log(a.length) // 13
+console.log(a.trim().length) // 13
+console.log(a[0]) // H
+
+let text = `Học về String
+cùng Thành`; // Dấu backtick () là ký hiệu nằm dưới phím Esctrên bàn phím, không phải dấu nháy đơn'hay dấu nháy kép"`.
+console.log(text);
+
+console.log('Học về String')
+
