df797d2679902e435ee5d43019ff8e1e66d1efcb
GTee2004 <giathanh232004@gmail.com>
2025-06-02 20:55:39 +0700

    Bai12 - While & Do-while

--- Bai11/Bai11.js
+++
@@ -1,9 +0,0 @@
-let arr = ['apple', 'banana', 'cherry'];
-
-for(let i=0; i<10; i++) {
-    console.log('Check variable i:', i);
-}
-
-for(let i=0; i<arr.length; i++) {
-    console.log('Check variable i:', arr[i]);
-}
---
+++ Bai12/Bai12.js
@@ -0,0 +1,16 @@
+let arr = ['MU', 'Liverpool', 'Chelsea', 'Arsenal', 'Tottenham'];
+
+// for(let i = 0; i < arr.length; i++) {
+//     console.log('Top: ', i+1, ' - ', arr[i]);
+// }
+// var i = 0;
+// while(i < arr.length) {
+//     console.log('Top: ', i+1, ' - ', arr[i]);
+//     i++;
+
+// }
+var i=0
+do{
+    console.log('Top: ', i+1, ' - ', arr[i]);
+    i++;
+}while(i < arr.length);
