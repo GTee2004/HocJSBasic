f61f74377694ba6e58150d02bf87b45363f9fc55
GTee2004 <giathanh232004@gmail.com>
2025-06-02 20:44:44 +0700

    Bai11 - For Loop

---
+++ Bai11/Bai11.js
@@ -0,0 +1,9 @@
+let arr = ['apple', 'banana', 'cherry'];
+
+for(let i=0; i<10; i++) {
+    console.log('Check variable i:', i);
+}
+
+for(let i=0; i<arr.length; i++) {
+    console.log('Check variable i:', arr[i]);
+}
