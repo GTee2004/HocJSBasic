22edbe8b3fb61dafc137fd373f43f39bc728d359
GTee2004 <giathanh232004@gmail.com>
2025-06-02 16:07:04 +0700

    Bai9 - Array

---
+++ Bai9/Bai9.js
@@ -0,0 +1,30 @@
+let a = {
+    name: 'Thanh',
+    age: 21
+
+}; // key: value
+
+console.log('Check data object:', a);
+console.log('My name\'s: ', a.name);
+console.log('My age is: ', a.age);
+
+// Array = [item1, item2, item3]
+let b = ['Manchester United', 'Chelsea', 'Real Madrid', 'Barcelona'];
+console.log('Check data array:', b);
+console.log('Type of b:', typeof b);
+console.log('My favorite football team is: ', b[0]);
+let family = [
+    {
+        name: 'Thanh',
+        age: 21
+    },
+    {
+        name: 'Hanh',
+        age: 20
+    },
+    {
+        name: 'Hieu',
+        age: 19
+    }
+]
+console.log('My family:', family);
