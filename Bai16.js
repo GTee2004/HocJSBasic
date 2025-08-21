8acad440384bb00cbe8c1ab351b966f68db1e30d
GTee2004 <giathanh232004@gmail.com>
2025-08-21 19:58:21 +0700

    Arrow Function

---
+++ Bai16.js
@@ -0,0 +1,11 @@
+console.log("Bai16.js loaded");
+
+// Arrow function
+const sum = (a, b) => {
+    return a + b;
+}
+
+console.log(sum(5, 10));
+// (function () {
+//     console.log("Ham tu chay");
+// })();
