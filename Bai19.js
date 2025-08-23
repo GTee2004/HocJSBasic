7f506f1c53cf6bd6a60ca79bc7883ca94452fc02
GTee2004 <giathanh232004@gmail.com>
2025-08-23 14:42:42 +0700

    Thêm, Sửa, Xóa array

--- Bai19.js
+++ Bai19.js
@@ -5,7 +5,15 @@ console.log("Hello World!")
 const names = ["Thành", "Trung", "Hòa"]
 
 console.log(names);
+console.log("0: " + names[0]);
+console.log("10: " + names[10]);
 
-const others = ["An", "Bình", "Cường", true, [1, 2 ,3]];
+names[2] = "Upadate name";
 
-console.log(others);
+console.log(names);
+
+names.push("Trần", "Gia"); // Thêm cuối
+names.unshift(null); // Thêm đầu
+names.pop(); // Xóa cuối
+names.shift(); // Xóa đầu
+console.log(names);
