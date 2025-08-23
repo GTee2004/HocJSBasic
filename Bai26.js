f3fcc0477d831cab55569d38d2995ac9eb31ae78
GTee2004 <giathanh232004@gmail.com>
2025-08-23 20:00:58 +0700

    DOMf

---
+++ Bai26.js
@@ -0,0 +1,10 @@
+console.log("Hello")
+
+const username = document.getElementById("username");
+
+const myDiv = document.querySelector(".hello");
+
+const mySpan = document.querySelectorAll(".hello");
+console.log(username);
+console.log(myDiv);
+console.log(mySpan);
