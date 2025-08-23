afd421d81593f851fbd9205733df97ac1b0ec182
GTee2004 <giathanh232004@gmail.com>
2025-08-23 14:37:08 +0700

    Array'

---
+++ Bai19.js
@@ -0,0 +1,11 @@
+console.log("Hello World!")
+
+// Array
+
+const names = ["Thành", "Trung", "Hòa"]
+
+console.log(names);
+
+const others = ["An", "Bình", "Cường", true, [1, 2 ,3]];
+
+console.log(others);
